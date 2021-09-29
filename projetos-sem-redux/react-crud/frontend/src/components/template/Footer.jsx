6a2035d89bc968e9f81e59c0bb85fd7ab74fe9db
Nando Piattelli <87387023+nandowl@users.noreply.github.com>
2021-09-28 21:29:13 -0300

    Add files via upload

---
+++ projetos-sem-redux/react-crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Nan<span className="text-danger">dowl </span>®</strong>
+        </span>
+    </footer>
