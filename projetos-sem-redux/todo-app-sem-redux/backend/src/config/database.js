326113651a9075e8084b15368c5fe6e607dd37eb
Nando Piattelli <87387023+nandowl@users.noreply.github.com>
2021-09-28 21:25:40 -0300

    Delete projetos-sem-redux directory

--- projetos-sem-redux/todo-app-sem-redux/backend/src/config/database.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise //mongoose está depreciado e pra tirar o warn do terminal é feito isso
-module.exports = mongoose.connect('mongodb://localhost/todo')
